7171fcd184c502cd5d401ea85667cd0ceafc8c7a
Ihimbru Kanyimi <87714194+Ihimbru-K@users.noreply.github.com>
2023-03-07 23:10:10 +0100

    Add files via upload

---
+++ Classes (initializing code).kt
@@ -0,0 +1,23 @@
+import kotlin.time.measureTimedValue
+
+/*
+Within the init block we change the value of fuelLevel based on the value of
+yearOfMake. Since this requires access to fuelLevel, it can’t be earlier than the
+declaration of fuelLevel
+ */
+
+fun main (){
+    class Car(val yearMade : Int, theColor : String)
+      var FuelLevel = 100
+
+    var color = theColor
+    set(value)
+        if(value.isBlank()){
+        throw RuntimeException("no empty value, please")
+    }
+    field = value
+    init {
+        if(yearMade < 2020){ FuelLevel = 90}
+    }
+
+}
